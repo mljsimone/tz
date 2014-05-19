28af30ae474ca4160b0c1b0a6bb8ad430a67db7e
Mariano Simone <mljsimone@gmail.com>
2014-05-18 23:48:54 -0300

    Moving route mappings to app/index.js

--- backend/app/index.js
+++ backend/app/index.js
@@ -11,6 +11,42 @@ tz.use(serveStatic(__dirname + "/../public"));
 var models = require("./models");
 var controllers = require("./controllers");
 
-var server = tz.listen(3000, function() {
-  console.log("Listening on %s:%d", server.address().address, server.address().port);
-});
+var users = controllers.users;
+//var timezones = controllers.timezones;
+
+tz.
+  post("/api/v1/users", users.create).
+  delete("/api/v1/users/:id", users.destroy).
+  get("/api/v1/users", users.findAll).
+  get("/api/v1/users/:id?", users.findOne).
+  put("/api/v1/users/:id", users.update);
+
+/*
+tz.
+  post("/api/v1/timezones", timezones.create).
+  delete("/api/v1/timezones/:id", timezones.destroy).
+  get("/api/v1/timezones", timezones.findAll).
+  get("/api/v1/timezones/:id?", timezones.findOne).
+  put("/api/v1/timezones/:id", timezones.update);
+*/
+
+models.sequelize.
+  sync({ force: true }).
+  success(function() {
+    models.User.
+      create({ username: 'bob', password: '1111' }).
+      success(function() { console.log("User bob was created."); }).
+      error(function() { console.log("Unable to create user bob."); });
+    
+    models.User.
+      create({ username: 'alice', password: '1111' }).
+      success(function() { console.log("User alice was created."); }).
+      error(function() { console.log("Unable to create user alice."); });
+      
+    var server = tz.listen(3000, function() {
+      console.log("Listening on %s:%d", server.address().address, server.address().port);
+    });
+  }).
+  error(function(error) {
+    throw new Error("Sequelize's sync failed: " + error);
+  });
